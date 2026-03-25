499434f4adafc7485c58f3d12e53519080371925
Lucas Verron <luk.verron24@gmail.com>
2026-03-25 15:39:34 +0100

    Affiche la liste de toutes les classes

--- tp9_sql_php/index.php
+++ tp9_sql_php/index.php
@@ -2,13 +2,25 @@
 
     require_once(__DIR__.'../model/pdo.php');
 
-    $resultat = $dbPDO->prepare("SELECT * FROM eleves");
-    $resultat ->execute();
-    $clients = $resultat->fetchAll();
-    foreach($clients as $client) {
+    $etudiant = $dbPDO->prepare("SELECT * FROM eleves");
+    $etudiant ->execute();
+    $res = $etudiant->fetchAll();
+    foreach($res as $re) {
     echo "<br>";
-    echo $client['Id_eleve']." ".$client['nom']." ".$client['prenom'];
+    echo $re['Id_eleve']." ".$re['nom']." ".$re['prenom'];
     }  
     //affiche la liste de tous les étudiants avec le nom et prénom
+
+
+
+    echo "<br>";
+    $classe = $dbPDO->prepare("SELECT * FROM classes");
+    $classe ->execute();
+    $bla = $classe->fetchAll();
+    foreach($bla as $bl) {
+    echo "<br>";
+    echo $bl['Id_Classes']." ".$bl['nom'];
+    } 
+    //Affiche la liste de toutes les classes 
 
 ?>
