65949eb613bb1f8e6e35f608127752e790cd3adc
Lucas Verron <luk.verron24@gmail.com>
2026-04-01 14:08:28 +0200

    partie 3

--- tp9_sql_php/nouv_etud.php
+++ tp9_sql_php/nouv_etud.php
@@ -5,9 +5,9 @@ require_once(__DIR__.'../model/pdo.php');
 
 $nom = htmlspecialchars(trim($_POST['nom']));
 $prenom = htmlspecialchars(trim($_POST['prenom']));
-$id_classe = htmlspecialchars(trim($_POST['id_classe']));
+$id_classe = intval(htmlspecialchars(trim($_POST['id_classe'])));
 
-$sql = "INSERT INTO eleves (nom,prenom,id_classe ) VALUES (:nom,:prenom,:id_classe)";
+$sql = "INSERT INTO eleves (nom,prenom,Id_Classes ) VALUES (:nom,:prenom,:id_classe)";
 $res = $dbPDO->prepare($sql);
 $res ->execute([
 	'nom' => $nom,
@@ -21,6 +21,6 @@ echo "<p>eleve <strong>" . $prenom . "</strong> a bien été ajoutée en base de
 ?>
 <html>
 <br>
-    <a href="../index.php">Retour à la page du formulaire</a>
+    <a href="./index.php">Retour à la page du formulaire</a>
 
 </html>
--- tp9_sql_php/nouvelle_matiere.php
+++ tp9_sql_php/nouvelle_matiere.php
@@ -5,11 +5,11 @@ require_once(__DIR__.'../model/pdo.php');
 
 $libelle = htmlspecialchars(trim($_POST['libelle']));
 
-$sql = "INSERT INTO matiere (libelle) VALUES (:libelle)";
+$sql = "INSERT INTO matiere (nom) VALUES (:libelle)";
 $res = $dbPDO->prepare($sql);
 $res ->execute([
 	'libelle' => $libelle
-]);
+]); 
 
 echo "<p>La matière <strong>" . $libelle . "</strong> a bien été ajoutée en base de données.</p>";
 
@@ -17,7 +17,7 @@ echo "<p>La matière <strong>" . $libelle . "</strong> a bien été ajoutée en 
 ?>
 <html>
 <br>
-    <a href="../index.php">Retour à la page du formulaire</a>
+    <a href="./index.php">Retour à la page du formulaire</a>
 
 </html>
 
